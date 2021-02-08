93c14007b141ab5153f94dcd1427599f89bcdafc
Natalia Gajardo <natty.gajardo.s@gmail.com>
2021-02-07 21:42:43 -0300

    heroku push

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= jquery
-//= jquery_ujs
+//
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery 
 //= require chartkick
 //= require highcharts
 //= require_tree .
